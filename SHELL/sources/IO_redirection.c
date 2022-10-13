a26a7e4c1e5b22c2dcc439f6e14090cd4d795d8e
Mohammed Rashad <84274861+Mohammed-Rashad-Nasr@users.noreply.github.com>
2022-10-13 23:56:33 +0200

    Add files via upload

---
+++ SHELL/sources/IO_redirection.c
@@ -0,0 +1,228 @@
+/**********************************************************************************************************************
+ ********************************************************************************************************************
+ ********************************************************************************************************************
+ *  FILE DESCRIPTION
+ *  -----------------------------------------------------------------------------------------------------------------*/
+/**        \file  IO_redirection.c
+ *
+ *        \brief  this file contains functions to control input output redirection
+ *
+ *      \details  IO_redirection.c 2.0v 
+ *                last edit ... 13 OCT 2022
+ *                author ...... mohammed rashad
+ *                
+ *                notes :
+ *                
+ *                - tab = 8 spaces 
+ *                - errors represented using errno and perror function
+ *
+ *
+ *********************************************************************************************************************
+ *********************************************************************************************************************
+ *********************************************************************************************************************/
+
+
+//******************* includes ***************************
+//********************************************************
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "header.h"      //my header file
+
+//********************************************************
+//********************************************************
+
+
+
+
+//*************** global vars ****************************
+//********************************************************
+
+int out, in, err, stdi, stde, stdo;   //variable to contain fds of redirection files and original files too
+
+//********************************************************
+//********************************************************
+
+
+
+
+
+
+
+//*********output redirection function *******************
+//********************************************************
+
+void changeOut(char *path)
+{
+
+    out = open(path, O_CREAT | O_WRONLY, 0755);  //open output file or creat it if it is not found in write only mode
+    if (out == -1 )
+    {
+            //******** error in open *******
+            
+            red();        
+            printf("failed to open the file %s \n",path);
+            perror("the error is ");
+            reset();
+            exit(1);
+            
+            //*******************************
+            
+    }
+    stdo = dup(fileno(stdout));                  //save the original stdout
+    if (stdo == -1 )
+    {
+            //******** error in dup *******
+            
+            red();        
+            printf("failed to creat copy of stdout \n");
+            perror("the error is ");
+            reset();
+            exit(1);
+            
+            //*******************************
+            
+    }
+                       
+    if (dup2(out, fileno(stdout)) == -1 )        //redirect the output
+    {
+            //******** error in dup2 *******
+            
+            red();        
+            printf("failed to redirect the output \n");
+            perror("the error is ");
+            reset();
+            exit(1);
+            
+            //*******************************
+            
+    }
+
+}
+
+//********************************************************
+//********************************************************
+
+
+
+
+
+
+//*********input redirection function ********************
+//********************************************************
+void changeIn(char *path)                               
+{
+
+    in = open(path, O_CREAT | O_RDONLY, 0755);                //open input file or creat it if it is not found in read only mode
+    if (in == -1 )
+    {
+            //******** error in open *******
+            
+            red();        
+            printf("failed to open the file %s \n",path);
+            perror("the error is ");
+            reset();
+            exit(1);
+            
+            //*******************************
+            
+    }
+    stdi = dup(fileno(stdin));                                //save the original stdin
+    if (stdi == -1 )
+    {
+            //******** error in dup *******
+            
+            red();        
+            printf("failed to creat copy of stdin \n");
+            perror("the error is ");
+            reset();
+            exit(1);
+            
+            //*******************************
+            
+    }
+    
+    if (dup2(in, fileno(stdin)) == -1 )                       //redirect the input
+    {
+            //******** error in dup2 *******
+            
+            red();        
+            printf("failed to redirect the input \n");
+            perror("the error is ");
+            reset();
+            exit(1);
+            
+            //*******************************
+            
+    }                                 
+}
+//********************************************************
+//********************************************************
+
+
+
+
+
+//*********reset output to stdout function ********************
+//*************************************************************
+void resetOut()
+{
+    fflush(stdin);
+    fflush(stdout);
+    
+    if (dup2(stdo, fileno(stdout)) == -1 )                       //redirect the output
+    {
+            //******** error in dup2 *******
+            
+            red();        
+            printf("failed to redirect the output to stdout \n");
+            perror("the error is ");
+            reset();
+            exit(1);
+            
+            //*******************************
+            
+    }                                 
+}
+//*************************************************************
+//*************************************************************
+
+
+
+
+//*********reset input to stdin function ********************
+//*************************************************************
+void resetIn()
+{
+    fflush(stdout);
+    fflush(stdin);
+    
+    
+    if (dup2(stdi, fileno(stdin)) == -1 )                       //redirect the input
+    {
+            //******** error in dup2 *******
+            
+            red();        
+            printf("failed to redirect the input to stdin \n");
+            perror("the error is ");
+            reset();
+            exit(1);
+            
+            //*******************************
+            
+    }                          
+}
+//*************************************************************
+//*************************************************************
+
+
+
+
+/**********************************************************************************************************************
+ *  END OF FILE: IO_redirection.c
+ *********************************************************************************************************************/
---
+++ SHELL/sources/environment_vars.c
@@ -0,0 +1,193 @@
+/**********************************************************************************************************************
+ ********************************************************************************************************************
+ ********************************************************************************************************************
+ *  FILE DESCRIPTION
+ *  -----------------------------------------------------------------------------------------------------------------*/
+/**        \file  environment_vars.c
+ *
+ *        \brief  this file contains functions to control and store the environment variable
+ *
+ *      \details  environment_vars.c 2.0v 
+ *                last edit ... 13 OCT 2022
+ *                author ...... mohammed rashad
+ *                
+ *                notes :
+ *                
+ *                - tab = 8 spaces 
+ *                - errors represented using errno and perror function
+ *                - note that shell is space sensetive x=5 and x = 5 are not equal 
+ *                  "don't separate variable name , value and equal operator by spaces"
+ *                - define the maximum values in the header file and it will be your responsibility to 
+ *                  don't exceed these limits
+ *
+ *
+ *********************************************************************************************************************
+ *********************************************************************************************************************
+ *********************************************************************************************************************/
+
+
+//******************* includes ***************************
+//********************************************************
+
+#include <string.h>
+#include <stdio.h>
+#include "header.h"    //my header file
+
+//********************************************************
+//********************************************************
+
+
+
+
+//*************** global vars ****************************
+//********************************************************
+
+int      varcount = 0;
+variable varArr[MAX_VARS_NUMBER];
+
+//********************************************************
+//********************************************************
+
+
+
+//********** function to search in the variables array for given name**********
+//*****************************************************************************
+
+int search(char * Name)
+{
+    int iterator = 0;
+
+    while (iterator < varcount) 
+    {
+	if (strcmp(varArr[iterator].name, Name) == 0) 
+        {
+
+	    return iterator;  //return location if found
+	}
+	
+        iterator++;  
+    }
+    return -1;   //return -1 if not found
+}
+
+//*****************************************************************************
+//*****************************************************************************
+
+
+
+
+
+//********** function to check if given string is variable or not using "="**********
+//***********************************************************************************
+
+int isenv(char *command)
+{
+    int iterator = 0;
+    while (command[iterator] != '\0') 
+    {
+	if (command[iterator] == '=') 
+        {
+	    return 1;  //if it is environment var return one
+	}
+        
+	iterator++;
+    }
+    return 0;   //if it is not environment var return zero
+
+}
+
+//***********************************************************************************
+//***********************************************************************************
+
+
+
+
+
+
+//********** function to get var in form of "name=value" and store it in array of structs**********
+//*************************************************************************************************
+void construct(char * element)
+{
+
+    char tmp1[strlen(element)];
+    strcpy   (tmp1,  element) ;
+    char *varname = strtok(tmp1, "=");    //get the string before = to check if it is already defined or not
+
+    if (search(varname) == -1)            
+    //if it is not defined creat new name and value structure element
+    //***************************************************************
+    {   
+	char tmp[strlen(element)];
+	strcpy(tmp, element);
+	char *div = strtok(tmp, "=");              //name
+	strcpy(varArr[varcount].name, div);
+	while (div != NULL) 
+        {
+	    strcpy(varArr[varcount].value, div);   //value
+	    div = strtok(NULL, "=");
+	}
+	varcount++;                                //increase number of vars 
+    } 
+    //***************************************************************
+    
+    else 
+    //if it is defined change the value only
+    //**************************************
+    {
+
+	char tmp[strlen(element)];
+	strcpy(tmp, element);
+	char *div = strtok(tmp, "=");                  
+
+	while (div != NULL) 
+        {
+	    strcpy(varArr[search(varname)].value, div);
+	    div = strtok(NULL, "=");
+	}
+
+    }
+    //**************************************
+}
+//*************************************************************************************************
+//*************************************************************************************************
+
+
+
+
+//********** function to count number of vars in forn "name=value" in given string line  **********
+//*************************************************************************************************
+int countenv(char * line)
+{
+
+    int iterator = 0;
+    int count = 0;
+    while (line[iterator] != '\0') 
+    {
+	if (line[iterator] == '=')
+	    count++;
+	
+        iterator++;
+    }
+    return count;
+}
+//*************************************************************************************************
+//*************************************************************************************************
+
+
+
+
+//*******function to list all vars defined **********
+//***************************************************
+void printvars()
+{
+    for (int iterator = 0; iterator < varcount; iterator++)
+	printf("%d  name : %s  value : %s\n", iterator, varArr[iterator].name,varArr[iterator].value);
+}
+//***************************************************
+//***************************************************
+
+
+
+/**********************************************************************************************************************
+ *  END OF FILE: environment_vars.c
+ *********************************************************************************************************************/
