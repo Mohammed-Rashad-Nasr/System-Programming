9f35271d1af3018d21ae270c8d90f5c9d8d197ce
Mohammed Rashad <84274861+Mohammed-Rashad-Nasr@users.noreply.github.com>
2022-10-13 23:34:54 +0200

    final versions

--- shell/environment_vars.c
+++ shell/environment_vars.c
@@ -1,85 +1,193 @@
+/**********************************************************************************************************************
+ ********************************************************************************************************************
+ ********************************************************************************************************************
+ *  FILE DESCRIPTION
+ *  -----------------------------------------------------------------------------------------------------------------*/
+/**        \file  environment_vars.c
+ *
+ *        \brief  this file contains functions to control and store the environment variable
+ *
+ *      \details  environment_vars.c 2.0v 
+ *                last edit ... 13 OCT 2022
+ *                author ...... mohammed rashad
+ *                
+ *                notes :
+ *                
+ *                - tab = 8 spaces 
+ *                - errors represented using errno and perror function
+ *                - note that shell is space sensetive x=5 and x = 5 are not equal 
+ *                  "don't separate variable name , value and equal operator by spaces"
+ *                - define the maximum values in the header file and it will be your responsibility to 
+ *                  don't exceed these limits
+ *
+ *
+ *********************************************************************************************************************
+ *********************************************************************************************************************
+ *********************************************************************************************************************/
+
+
+//******************* includes ***************************
+//********************************************************
+
 #include <string.h>
 #include <stdio.h>
-#include "header.h"
+#include "header.h"    //my header file
 
-int varcount = 0;
-variable vararr[100];
-int search(char *s)
+//********************************************************
+//********************************************************
+
+
+
+
+//*************** global vars ****************************
+//********************************************************
+
+int      varcount = 0;
+variable varArr[MAX_VARS_NUMBER];
+
+//********************************************************
+//********************************************************
+
+
+
+//********** function to search in the variables array for given name**********
+//*****************************************************************************
+
+int search(char * Name)
 {
-    int i = 0;
+    int iterator = 0;
 
-    while (i < varcount) {
-	if (strcmp(vararr[i].name, s) == 0) {
+    while (iterator < varcount) 
+    {
+	if (strcmp(varArr[iterator].name, Name) == 0) 
+        {
 
-	    return i;
+	    return iterator;  //return location if found
 	}
-	i++;
+	
+        iterator++;  
     }
-    return -1;
+    return -1;   //return -1 if not found
 }
 
-int isenv(char *c)
-{
-    int i = 0;
-    while (c[i] != '\0') {
-	if (c[i] == '=') {
-	    return 1;
+//*****************************************************************************
+//*****************************************************************************
 
+
+
+
+
+//********** function to check if given string is variable or not using "="**********
+//***********************************************************************************
+
+int isenv(char *command)
+{
+    int iterator = 0;
+    while (command[iterator] != '\0') 
+    {
+	if (command[iterator] == '=') 
+        {
+	    return 1;  //if it is environment var return one
 	}
-	i++;
+        
+	iterator++;
     }
-    return 0;
+    return 0;   //if it is not environment var return zero
 
 }
 
-void construct(char *c)
+//***********************************************************************************
+//***********************************************************************************
+
+
+
+
+
+
+//********** function to get var in form of "name=value" and store it in array of structs**********
+//*************************************************************************************************
+void construct(char * element)
 {
 
-    char tempo1[strlen(c)];
-    strcpy(tempo1, c);
-    char *div1 = strtok(tempo1, "=");
+    char tmp1[strlen(element)];
+    strcpy   (tmp1,  element) ;
+    char *varname = strtok(tmp1, "=");    //get the string before = to check if it is already defined or not
 
-    if (search(div1) == -1) {
-	char tempo[strlen(c)];
-	strcpy(tempo, c);
-	char *div = strtok(tempo, "=");
-	strcpy(vararr[varcount].name, div);
-	while (div != NULL) {
-	    strcpy(vararr[varcount].value, div);
+    if (search(varname) == -1)            
+    //if it is not defined creat new name and value structure element
+    //***************************************************************
+    {   
+	char tmp[strlen(element)];
+	strcpy(tmp, element);
+	char *div = strtok(tmp, "=");              //name
+	strcpy(varArr[varcount].name, div);
+	while (div != NULL) 
+        {
+	    strcpy(varArr[varcount].value, div);   //value
 	    div = strtok(NULL, "=");
 	}
-	varcount++;
-    } else {
-	char tempo[strlen(c)];
-	strcpy(tempo, c);
-	char *div = strtok(tempo, "=");
+	varcount++;                                //increase number of vars 
+    } 
+    //***************************************************************
+    
+    else 
+    //if it is defined change the value only
+    //**************************************
+    {
 
-	while (div != NULL) {
-	    strcpy(vararr[search(div1)].value, div);
+	char tmp[strlen(element)];
+	strcpy(tmp, element);
+	char *div = strtok(tmp, "=");                  
+
+	while (div != NULL) 
+        {
+	    strcpy(varArr[search(varname)].value, div);
 	    div = strtok(NULL, "=");
 	}
 
     }
-
-
+    //**************************************
 }
+//*************************************************************************************************
+//*************************************************************************************************
 
-int countenv(char *c)
+
+
+
+//********** function to count number of vars in forn "name=value" in given string line  **********
+//*************************************************************************************************
+int countenv(char * line)
 {
 
-    int i = 0;
+    int iterator = 0;
     int count = 0;
-    while (c[i] != '\0') {
-	if (c[i] == '=')
+    while (line[iterator] != '\0') 
+    {
+	if (line[iterator] == '=')
 	    count++;
-	i++;
+	
+        iterator++;
     }
     return count;
 }
+//*************************************************************************************************
+//*************************************************************************************************
 
+
+
+
+//*******function to list all vars defined **********
+//***************************************************
 void printvars()
 {
-    for (int ii = 0; ii < varcount; ii++)
-	printf("%d  name : %s  value : %s\n", ii, vararr[ii].name,
-	       vararr[ii].value);
+    for (int iterator = 0; iterator < varcount; iterator++)
+	printf("%d  name : %s  value : %s\n", iterator, vararr[iterator].name,varArr[iterator].value);
 }
+//***************************************************
+//***************************************************
+
+
+
+/**********************************************************************************************************************
+ *  END OF FILE: environment_vars.c
+ *********************************************************************************************************************/
